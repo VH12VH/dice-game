4407d30af67481f46141a8fdecc576ec1c9d2ab4
Vincent Haughton <104997238+VH12VH@users.noreply.github.com>
2022-05-05 14:57:52 +0000

    game working

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ player2 = diceRoll()
 
 print (f"Player 1 rolls a {player1}")
 print (f"player 2 rolls a {player2}")
+
+if player1 == player2:
+  print(f"you both got a {player1}")
+elif player1 > player2:
+  print (f"Player 1 wins! ({player1} over {player2})")
+else:
+  print (f"Player 2 wins! ({player2} over {player1})")
